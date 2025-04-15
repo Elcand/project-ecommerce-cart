6bdb8be779a0c8f74fd184d60710e0cddfeea3b1
Elcand <tjandraraharja99@gmail.com>
2025-04-15 09:29:28 +0700

    Project - Working with Admin Dashboard

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,12 +1,13 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h4 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Profile Information') }}
-        </h2>
+        </h4>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __("Update your account's profile information and email address.") }}
         </p>
+        <hr>
     </header>
 
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
@@ -56,7 +57,7 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
+                    class="alert alert-success text-sm text-success font-medium mt-3"
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
